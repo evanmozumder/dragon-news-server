2b91e8c54c4f30a44c3c8eec9bb2fc663cd65236
evanmozumder <evanmozumder5@gmail.com>
2023-01-12 19:38:10 +0600

    server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+
+app.get("/", (req, res) => {
+  res.send("dragon news server is ready");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
